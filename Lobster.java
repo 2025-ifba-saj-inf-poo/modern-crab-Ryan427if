112179323d8cafab0f7f038426d6d032eecde5ed
Hildemar0034 <20231tadssaj0034@ifba.edu.br>
2025-08-25 11:38:33 -0300

    Fim_Decapoda.

---
+++ Lobster.java
@@ -0,0 +1,8 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Lobster extends Decapoda
+{
+    public Lobster(){        
+        super("A","S", 550,550 );
+    }    
+}
